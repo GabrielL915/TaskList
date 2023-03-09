06d1dae0698aa6bb80c90e7d8ae4e0937043200f
gabriel gomes <gabrielluizgomes1@gmail.com>
2023-03-09 00:40:56 -0300

    add funcionalidade de criar Taks

---
+++ app.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+function loadEventListener() {
+    //peando o evento de submit do add task
+    form.addEventListener('submit', (event) => {
+        //checando de o valor mandado é vazio
+        if (taskInput.value === '') {
+            alert('Add a task');
+        }
+    
+        //criando um lista com uma class collection-item
+        const li = document.createElement('li');
+        li.className = 'collection-item';
+    
+        //add o valor mandado para a li
+        li.appendChild(document.createTextNode(taskInput.value));
+    
+        //criando um link com a classe delete...
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content'
+    
+        //criando a tag i dentro do link <a><i></i></a>
+
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+    
+        //link é adicionado como um filho do elemento li 
+        li.appendChild(link);
+
+        //li é add como um filho de tasklist
+        taskList.appendChild(li);
+        
+        //zera o valor do imput
+        taskInput.value = '';
+    
+        event.preventDefault()
+    
+    });
+}
+
+loadEventListener();
+
